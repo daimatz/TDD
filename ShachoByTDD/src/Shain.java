d44483787684617b34f6461e5c374ba48e7ab999
dai <dai@daimatz.net>
2012-03-28 05:56:33 +0900

    pass test (Green)

--- ShachoByTDD/src/Shain.java
+++ ShachoByTDD/src/Shain.java
@@ -1,6 +1,20 @@
 
 public class Shain {
+	public static final int TANTO = 0;
+	public static final int SHUNIN = 1;
+	
+	public final int type;
+	
+	public Shain(int type) {
+		this.type = type;
+	}
+	
 	public String standup() {
-		return "担当が普通に起立しました";
+		if (type == TANTO) {
+			return "担当が普通に起立しました";
+		} else if (type == SHUNIN) {
+			return "主任がすばやく立ちました";
+		}
+		throw new IllegalStateException();
 	}
 }
--- ShachoByTDD/src/ShainTest.java
+++ ShachoByTDD/src/ShainTest.java
@@ -6,12 +6,12 @@ public class ShainTest {
 
 	@Test
 	public void standup() {
-		Shain shain = new Shain();
+		Shain shain = new Shain(Shain.TANTO);
 		assertEquals("担当が普通に起立しました", shain.standup());
 	}
 
 	public void standupForShunin() {
-		Shain shain = new Shain();
+		Shain shain = new Shain(Shain.SHUNIN);
 		assertEquals("主任がすばやく立ちました", shain.standup());
 	}
 }
